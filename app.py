469a5c22ff15f5df6e9d38f93d18969bb333bfd8
MANISH YADAV <yadavmanishmky2004@gmail.com>
2026-06-17 20:13:57 +0530

    Update API key to placeholder
    Replaced the API key with a placeholder for security.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # ==============================
 
 client_ai = Groq(
-    api_key="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"
+    api_key="your API_KEY"
 )
 
 # ==============================
